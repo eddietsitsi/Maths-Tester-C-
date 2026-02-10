9ceec0ad00d6a07ab18ecb52f8f8cae6e013ef3d
Eddie Tsitsi <249027338+eddietsitsi@users.noreply.github.com>
2026-02-10 23:20:28 +0200

    1

---
+++ MAths tester correct/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace MAths_tester_correct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 350);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Result:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Value 1:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 272);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Value 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(176, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(515, 91);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Maths Tester";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(459, 350);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(93, 32);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "label5";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(459, 201);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(140, 22);
+            this.txtValue1.TabIndex = 5;
+            this.txtValue1.TextChanged += new System.EventHandler(this.txtValue1_TextChanged);
+            this.txtValue1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValue1_KeyPress);
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(459, 272);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(140, 22);
+            this.txtValue2.TabIndex = 6;
+            this.txtValue2.TextChanged += new System.EventHandler(this.txtValue2_TextChanged);
+            this.txtValue2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtValue2_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(246, 434);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 47);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(390, 434);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(115, 47);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(525, 437);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(115, 47);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(304, 153);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Enter value in textboxes to compute";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(895, 570);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " f";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MAths tester correct/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MAths_tester_correct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       private void Form1_Load(object sender, EventArgs e)
+        {
+
+            lblResult.Visible = false;
+        }
+             
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string Value1 = txtValue1.Text;
+            string Value2 = txtValue2.Text;
+
+            int isNum1;
+            int isNum2;
+            Double AddR;
+
+            if (int.TryParse(Value1, out isNum1) && int.TryParse(Value2, out isNum2))
+            {
+                AddR = (isNum1+isNum2);
+                lblResult.Visible = true;
+
+                if (isNum1 > 0 && isNum2 > 0)
+                {
+                    lblResult.ForeColor = Color.Purple;
+                    lblResult.Text = AddR.ToString();
+                }
+                else
+                {
+                    lblResult.ForeColor = Color.Red;
+                    lblResult.Text = AddR.ToString();
+                    MessageBox.Show("Value must be numeri and >0");
+
+                }
+            }
+          
+
+            
+            
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            string Value1 = txtValue1.Text;
+            string Value2 = txtValue2.Text;
+            
+            lblResult.ForeColor = Color.Purple;
+            int isNum1;
+            int isNum2;
+            Double MulR;
+
+            if (int.TryParse(Value1, out isNum1) && int.TryParse(Value2, out isNum2))
+            {
+                MulR = (isNum1*isNum2);
+                lblResult.Visible = true;
+
+                if (isNum1>0 && isNum2>0)
+                {
+                  lblResult.ForeColor = Color.Purple;
+                  lblResult.Text = MulR.ToString();
+                }
+                else
+                {
+                    lblResult.ForeColor = Color.Red;
+                    lblResult.Text = MulR.ToString();
+                    MessageBox.Show("Value must be numeri and >0");
+
+                }
+                
+
+            }
+           
+        }
+
+        private void txtValue1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtValue2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            
+            
+            txtValue1.Clear();
+            txtValue2.Clear();
+            lblResult.ForeColor = Color.Yellow;
+            lblResult.Text = "0";            
+        }
+
+        private void txtValue1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+            (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                MessageBox.Show("Enter numeric values only");
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+                MessageBox.Show("Enter numeric values only");
+            }
+        }
+
+        private void txtValue2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+          (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+                MessageBox.Show("Value must be numeri and >0");
+                
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+                MessageBox.Show("Value must be numeri and >0");
+            }
+
+        }
+
+       
+    }
+}
